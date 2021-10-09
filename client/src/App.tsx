11f45e53cad512869dc9ab2e389cb617f3b0faa1
Roman <rebrovdev@gmail.com>
2021-10-09 21:10:34 +0300

    1.2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './SASS/default.sass'
 
 const App : React.FC  = () => {
     return (
-        <div>
-
-        </div>
+        <>
+                
+        </>
     )
 }
 
--- client/src/redux/redusers/RootReduser.ts
+++ client/src/redux/redusers/RootReduser.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import Cart from './cart'
+import User from './user'
 
 const RootReduser = combineReducers({
     Cart,
+    User
 })
 
 export default RootReduser;
