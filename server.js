48837f0cb27f2d2a633b11ccb3a0a1c4ee44fac9
Roman <rebrovdev@gmail.com>
2021-10-10 01:33:34 +0300

    deploy7

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static(path.resolve(__dirname, './client/build')));
 
 const fine  = "OK!";
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './client/build',  './index.html'))
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build',  './build/index.html'))
+});
 
 
 
