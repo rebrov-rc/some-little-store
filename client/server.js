c6bce1b0b4fea321cf443e79ac41f1c6a67dca90
Roman <rebrovdev@gmail.com>
2021-10-11 09:53:35 +0300

    d5.0

--- client/server.js
+++ client/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+const fine  = "OK!";
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build',  './index.html'))
+    res.sendFile(path.join(__dirname, 'build',  'index.html'))
 });
 
 
